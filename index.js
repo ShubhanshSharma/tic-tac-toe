32f622ef123bfceb4d3d7ed487fb44c4eaeedf63
shubhansh <shubhanshsharma030604@gmail.com>
2023-10-11 10:19:53 +0530

    don3

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const boxes = document.querySelectorAll(".box");
 const info = document.querySelector(".header");
 const newGame = document.querySelector(".btn");
-
+const X = document.querySelector(".scoreX");
+const O = document.querySelector(".scoreY");
 let currentPlayer;
 let gameGrid;
 let n; 
+let x = 0;
+let o = 0;
+X.innerHTML =  `${x}`;
+O.innerHTML = `${o}`;
+let p;
 
 const winningPositions = [
     [0,1,2],
@@ -13,10 +19,40 @@ const winningPositions = [
     [3,4,5],
     [6,7,8],
     [2,4,6],
-    [4,1,7],
-    [2,5,8],
-    [4,1,7],
+    [1,4,7],
+    [2,5,8]
 ];
+
+function checkWin(){
+    winningPositions.forEach(position => {
+        if((gameGrid[position[0]] != "")  &&  
+            (gameGrid[position[0]] == gameGrid[position[1]] && gameGrid[position[1]] == gameGrid[position[2]] )){
+                // disable clicking pointer event 
+                boxes.forEach((box) => {
+                    box.style.pointerEvents = "none";
+                });
+                //score
+                if (gameGrid[position[0]] === "X") {
+                    x++;
+                    console.log("incremented X");
+                    X.innerHTML = `${x}`;
+                    console.log(x);
+                } 
+                if(gameGrid[position[0]] === "O"){
+                    o++;
+                    console.log("incremented O");
+                    O.innerHTML = `${o}`;
+                    console.log(o);
+                }
+                boxes[position[0]].style.backgroundColor = "#00800080";
+                boxes[position[1]].style.backgroundColor = "#00800080";
+                boxes[position[2]].style.backgroundColor = "#00800080";
+                
+                console.log(`won by ${currentPlayer}`);
+                p = 1;
+        }
+    });
+}
 
 //funtion to initialize game
 function initGame(){
@@ -24,8 +60,11 @@ function initGame(){
     gameGrid = ["","","","","","","","","",];
     newGame.style.display = "none";
     info.innerHTML =   `Current Player-${currentPlayer}`;
-    boxes.forEach((box, index) => {
-      box.innerHTML = "";
+    boxes.forEach((box) => {
+        box.style.pointerEvents = "auto";
+        box.innerHTML = "";
+        box.style.backgroundColor = "transparent";
+        p=0;
     });
     n = 0;
 }
@@ -40,9 +79,12 @@ function handleClick(index) {
       n = n + 1;
       if(n != 0)
         newGame.style.display = "flex";
-      if(checkWin())
+    checkWin();
+      if(p!=0){
+         info.innerHTML =   `${currentPlayer} Player Wins The Game`;
          return;
-      else sswitch();
+        }
+      else {sswitch();}
     }
 }
 
@@ -55,7 +97,6 @@ boxes.forEach((box, index) => {
 
 function sswitch(){
     if(currentPlayer === "X"){
-        console.log(currentPlayer);
         currentPlayer = "O";
         console.log(currentPlayer);
         info.innerHTML = `Current Player-${currentPlayer}`;
@@ -69,9 +110,7 @@ function sswitch(){
 
 newGame.addEventListener("click", initGame);
 
-function checkWin(){
 
-}
 
 
 
